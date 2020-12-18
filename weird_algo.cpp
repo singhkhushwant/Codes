8c3f9c198057cd86bba5cba4cc3284b9e04541f0
Khushwant Singh <50532309+khushwantsinghait@users.noreply.github.com>
2020-12-19 03:18:30 +0530

    weird ALGO problem solution.

---
+++ weird_algo.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define len(x) x.size()
+#define max3(a,b,c) man(a,man(b,c))
+#define min3(a,b,c) min(a,min(b,c))
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int32_t main()
+{
+    speed
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+
+    ll num;
+    cin>>num;
+    while(num!=1)
+    {
+        cout<<num<<" ";
+        if(num%2==0)
+          num/=2;
+        else
+          num=1+num*3;
+    }
+    cout<<1;
+    return 0;
+
+}
