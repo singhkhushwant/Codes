2a2945b4aa6a88240428cac086f8228edd97c1dc
Khushwant Singh <50532309+khushwantsinghait@users.noreply.github.com>
2020-12-21 02:51:00 +0530

    Operator Overloading.

--- FractionClass.cpp
+++ FractionClass.cpp
@@ -1,34 +1,62 @@
 using namespace std;
 class Fraction{
-private:
-  int const numerator;
-  int const denominator;
-  int const& x;
 public:
-  void getnumerator() const{
+  int  numerator;
+  int  denominator;
+  void getnumerator() {
     cout<<"numerator: "<<numerator<<endl;
   }
-  void getdenominator() const{
+  void getdenominator() {
     cout<<"denominator: "<<denominator<<endl;
   }
-  void getx() const{
-    cout<<"x: "<<x<<endl;
-  }
-  Fraction(int numerator, int denominator):numerator(numerator), denominator(denominator),x(this->denominator)
+  Fraction(int numerator, int denominator)
   {
     if(denominator==0)
     {
       cout<<"Wrong denominator"<<endl;
       return;
     }
-    //this->numerator=numerator;
-    //this->denominator=denominator;
+    this->numerator=numerator;
+    this->denominator=denominator;
   }
-  void display() const
+  void display()
   {
     cout<<"Fraction is: "<<numerator<<"/"<<denominator<<endl;
   }
-  /*
+  Fraction operator+(Fraction f)
+  {
+    int lcm=denominator * f.denominator;
+    int x=lcm/denominator;
+    int y=lcm/f.denominator;
+    int num=(x*numerator)+(y*f.numerator);
+    Fraction tf(num,lcm);
+    tf.simplify();
+    return tf;
+  }
+  //pre
+  Fraction& operator++()
+  {
+    this->numerator=this->numerator+this->denominator;
+    return (*this);
+  }
+  //post
+  Fraction operator++(int)
+  {
+    Fraction fnew(numerator,denominator);
+    numerator+=denominator;
+    simplify();
+    return fnew;
+
+  }
+  void operator+=(Fraction f)
+  {
+    int lcm=denominator * f.denominator;
+    int x=lcm/denominator;
+    int y=lcm/f.denominator;
+    numerator=(x*numerator)+(y*f.numerator);
+    denominator=lcm;
+    simplify();
+  }
   void simplify()
   {
     int gcd=1;
@@ -41,6 +69,7 @@ public:
     numerator=numerator/gcd;
     denominator=denominator/gcd;
   }
+  /*
   void add(Fraction const& f)
   {
     int lcm=denominator * f.denominator;
--- FractionMain.cpp
+++ FractionMain.cpp
@@ -12,7 +12,7 @@ int32_t main()
   freopen("output.txt","w",stdout);
   #endif
 
-  Fraction f(10,0);
+/*
   Fraction f1(15,15);
   f1.display();
   Fraction f2(100,10);
@@ -27,5 +27,19 @@ int32_t main()
   f3.getnumerator();
   f3.getdenominator();
   f3.getx();
+  */
+  Fraction f1(5,1);
+  Fraction f2(6,1);
+  Fraction f4=f1+f2;
+  ++f4;
+  Fraction f5=++(++f4);
+  f4.display();
+  f5.display();
+  Fraction f6=f4++;
+  f4.display();
+  f6.display();
+  f6+=f1;
+  f1.display();
+  f6.display();
   return 0;
 }
